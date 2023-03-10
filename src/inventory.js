9514c79be5c22e464d1c3c0b23a0eb2566039c94
ganesh4946 <122338021+ganesh4946@users.noreply.github.com>
2023-03-10 22:19:11 +0530

    init

--- src/inventory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const inventorySchema = new Schema({
-  productId: { type: Number, required: true },
-  quantity: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Inventory', inventorySchema);
